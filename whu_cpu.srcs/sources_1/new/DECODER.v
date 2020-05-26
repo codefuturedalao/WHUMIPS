27eefecc1742363f1b0f9c17181dc074e1bba936
jacksonSang <985438046@qq.com>
2020-05-27 00:37:48 +0800

    just create a vivado project and add first file decoder for testing
    1. the decoder file has nothing just for testing

---
+++ whu_cpu.srcs/sources_1/new/DECODER.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/27/2020 12:33:19 AM
+// Design Name: 
+// Module Name: DECODER
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module DECODER(
+    input [31:0] i_inst,
+    output o_reg1_read
+    );
+endmodule
