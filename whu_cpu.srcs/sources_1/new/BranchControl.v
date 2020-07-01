736e2957fb3e782c34096bc4c9d16c97f28e18cb
jacksonSang <985438046@qq.com>
2020-07-01 21:02:56 +0800

    Finish BranchControl part in ex stage

--- whu_cpu.srcs/sources_1/new/BranchControl.v
+++ whu_cpu.srcs/sources_1/new/BranchControl.v
@@ -1,4 +1,5 @@
 `timescale 1ns / 1ps
+`include "defines.v"
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
@@ -21,6 +22,29 @@
 
 
 module BranchControl(
-
+	input wire i_jump,
+	input wire i_jump_src,
+	input wire i_branch,
+	input wire [`INST_ADDR_WIDTH] i_pc,
+	input wire [25:0] i_imm26,
+	input wire [`REG_WIDTH] i_jump_reg_data,
+	input wire i_branch_flag,
+	output wire o_jump_branch,
+	output reg [`INST_ADDR_WIDTH] o_jump_branch_pc
     );
+	assign o_jump_branch = i_jump & i_branch & i_branch_flag;	
+	always
+		@(i_jump, i_jump_src, i_branch, i_pc, i_imm26, i_jump_reg_data) begin
+			if(i_jump == `IS_JUMP) begin
+				if(i_jump_src == `JUMP_FROM_REG) begin
+					o_jump_branch_pc <= i_jump_reg_data;
+				end	
+				else begin
+					o_jump_branch_pc <= {i_pc[31:28], i_imm26, 2'b00};
+				end
+			end 
+			else begin 
+				o_jump_branch_pc <= i_pc + {{14{i_imm26[15]}}, i_imm26, 2'b00};
+			end
+		end
 endmodule
