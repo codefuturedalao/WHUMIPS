7bc8e90e5569ddb1a5b7288045cac14091738d55
jacksonSang <985438046@qq.com>
2020-07-01 20:34:07 +0800

    Finish ALU and MUX1 in ex stage
    Do:
    1. ALU
    2. MUX1
    Undo:
    1. the HILO,CP0,syscall part
    2. there is a warning named "Warning: empty statement in sequential block"

--- whu_cpu.srcs/sources_1/new/MUX1.v
+++ whu_cpu.srcs/sources_1/new/MUX1.v
@@ -1,4 +1,5 @@
 `timescale 1ns / 1ps
+`include "defines.v"
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
@@ -21,6 +22,37 @@
 
 
 module MUX1(
-
+	input wire [1:0] i_forwardA,
+	input wire [1:0] i_forwardB,
+	input wire [`REG_WIDTH] i_mem_alu_result,
+	input wire [`REG_WIDTH] i_wb_reg3_data,
+	input wire [`REG_WIDTH] i_reg1_data,
+	input wire [`REG_WIDTH] i_reg2_data,
+	output reg [`REG_WIDTH] o_reg1_data,
+	output reg [`REG_WIDTH] o_reg2_data
     );
+	always
+		@(i_forwardA,i_forwardB,i_mem_alu_result,i_wb_reg3_data,i_reg1_data,i_reg2_data) begin
+			//reg1_data
+			if(i_forwardA == 2'b00) begin
+				o_reg1_data <= i_reg1_data;
+			end
+			else if(i_forwardA == 2'b01) begin
+				o_reg1_data <= i_mem_alu_result;
+			end
+			else if(i_forwardA == 2'b10) begin
+				o_reg1_data <= i_wb_reg3_data;
+			end
+			//reg2_data
+			if(i_forwardB == 2'b00) begin
+				o_reg2_data <= i_reg2_data;
+			end
+			else if(i_forwardB == 2'b01) begin
+				o_reg2_data <= i_mem_alu_result;
+			end
+			else if(i_forwardB == 2'b10) begin
+				o_reg2_data <= i_wb_reg3_data;
+			end
+		end
+
 endmodule
