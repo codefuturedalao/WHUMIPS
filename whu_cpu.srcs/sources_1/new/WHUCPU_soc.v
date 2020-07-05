2caae9266e18d685b064d734c6d2e19d1cb5b9fb
jacksonSang <985438046@qq.com>
2020-07-05 22:12:11 +0800

    Finish WHUCPU_soc.v

---
+++ whu_cpu.srcs/sources_1/new/WHUCPU_soc.v
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+`include "define.v"
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/05/2020 09:57:31 PM
+// Design Name: 
+// Module Name: WHUCPU_soc
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module WHUCPU_soc(
+	input wire i_sys_clk,
+	input wire i_sys_rst
+    );
+
+	wire [`REG_WIDTH] dmem_idata;
+	wire [`REG_WIDTH] imem_data;
+	wire [`INST_ADDR_WIDTH] imem_addr;
+	wire [`REG_WIDTH] dmem_addr;
+	wire [`REG_WIDTH] dmem_odata;
+	wire [3:0] dmem_wen;
+	wire dmem_en;
+	WHUCPU my_whucpu(
+			.i_clk(i_sys_clk), .i_rst(i_sys_rst), .i_dmem_data(dmem_odata), .i_imem_data(imem_data),
+
+			.o_imem_addr(imem_addr), .o_dmem_addr(dmem_addr), .o_dmem_data(dmem_idata), .o_dmem_wen(dmem_wen), .o_dmem_en(dmem_en)
+	);
+	IMEM my_imem(
+			.i_pc(imem_addr), .o_inst(imem_data)
+	);
+
+	DMEM my_dmem(
+			.i_clk(i_sys_clk), .i_addr(dmem_addr), .i_data(dmem_idata), .i_mem_en(dmem_en), .i_mem_wen(dmem_wen),
+
+			.o_data(dmem_odata)
+	);
+endmodule
